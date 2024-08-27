10c7cb2c82f60324391fa193791d18eb0f127f26
prajyotdhoke7 <157983877+prajyotdhoke7@users.noreply.github.com>
2024-08-27 16:01:08 +0530

    .

---
+++ IF-ELSE_02/Code2.dart
@@ -0,0 +1,41 @@
+void main() {
+  int color = 1;
+  switch (color) {
+    case 1:
+      print("Voilet");
+    case 2:
+      print("Indigo");
+    case 3:
+      print("Blue");
+    case 4:
+      print("Green");
+    case 5:
+      print("Yellow");
+    case 6:
+      print("Orange");
+    case 7:
+      print("Red");
+    default:
+      print("Enter valid color");
+  }
+
+  int color1 = 2;
+  switch (color1) {
+    case 1:
+      print("Voilet");
+    case 2:
+      print("Indigo");
+    case 3:
+      print("Blue");
+    case 4:
+      print("Green");
+    case 5:
+      print("Yellow");
+    case 6:
+      print("Orange");
+    case 7:
+      print("Red");
+    default:
+      print("Enter valid color");
+  }
+}
---
+++ IF-ELSE_02/Code3.dart
@@ -0,0 +1,11 @@
+void main() {
+  int num = 25;
+  num >= 30 && num <= 50
+      ? print("Number is in Valid Range")
+      : print("Invalid Number");
+
+  int num1 = 45;
+  num1 >= 30 && num1 <= 50
+      ? print("Number is in Valid Range")
+      : print("Invalid Number");
+}
