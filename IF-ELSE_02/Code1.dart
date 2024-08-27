f1f38fddd57c0d23a2cff2333332b7862bc95406
prajyotdhoke7 <157983877+prajyotdhoke7@users.noreply.github.com>
2024-08-27 14:07:42 +0530

    .

---
+++ IF-ELSE_02/Code1.dart
@@ -0,0 +1,13 @@
+void main() {
+  int ramSize = 4;
+  if (ramSize < 8)
+    print("can't run flutter Project");
+  else
+    print("can run flutter Project");
+
+  int ramSize1 = 8;
+  if (ramSize1 < 8)
+    print("can't run flutter Project");
+  else
+    print("can run flutter Project");
+}
