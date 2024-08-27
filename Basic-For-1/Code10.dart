8753e5ba3facb87ca2cfa91da3eab8731545640e
prajyotdhoke7 <157983877+prajyotdhoke7@users.noreply.github.com>
2024-08-28 00:22:17 +0530

    completed 3

---
+++ Basic-For-1/Code10.dart
@@ -0,0 +1,6 @@
+void main() {
+  double prod = 1;
+  for (int i = 1; i <= 10; i++) prod = prod * i;
+
+  print("product of First 10 numbers is $prod");
+}
---
+++ Basic-For-1/Code9.dart
@@ -0,0 +1,6 @@
+void main() {
+  int sum = 0;
+  for (int i = 1; i <= 10; i++) sum = sum + i;
+
+  print("Sum of First 10 Numbers is $sum");
+}
